2c2ef98bad3c27840ca423fa30bbce8f9ba72b22
Connley Farquhar <cfarq2003@gmail.com>
2025-05-18 14:50:58 +0100

    Corrected logout redirect

--- pages/logout.php
+++ pages/logout.php
@@ -4,6 +4,6 @@ session_unset();
 session_destroy(); 
 
 // Redirect the end user to index.php upon logout buttion being clicked.
-header("Location: http://localhost/DTS/index.php");
+header("Location: http://localhost/Sheetless/index.php");
 exit;
 ?>
